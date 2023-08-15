32b47fdda50cb5b1f22fc3e2d997ed79baf48c76
WilcoApp <hello@wilco.gg>
2023-08-15 09:04:52 +0000

    Dataseeding

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -40,17 +40,9 @@ router.get("/", auth.optional, function(req, res, next) {
   var query = {};
   var limit = 100;
   var offset = 0;
-  var title = "";
-  
+
   if (typeof req.query.limit !== "undefined") {
     limit = req.query.limit;
-    
-  }
-  if (typeof req.query.title !== "undefined") {
-    title = req.query.title;
-    console.log(title);
-    query.title = {$regex : title};
-    
   }
 
   if (typeof req.query.offset !== "undefined") {
